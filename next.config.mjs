ef1c19d66de94bca98627de43c767e558488a731
Nicolas <nicolas.dev.fullstack@gmail.com>
2024-08-13 18:18:38 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const isDeployed = process.env.IS_DEPLOYED != null ? process.env.IsDeployed : fa
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
-    basePath: process.env.BASE_PATH || '',
     assetPrefix: isDeployed ? process.env.SUB_DOMAIN : '',
     compress: true,
 };
